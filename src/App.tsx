6dea4359ca5d443db5f510943e8cd1278f74abed
Dar-ya218 <dasharoura13465@gmail.com>
2023-06-16 11:14:17 +0200

    ejercicio_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import './App.css'
+import "./App.css";
+import { checkboxData } from "./component/checkboxData";
+import {useState} from "react"
 
 function App() {
+  const [checkboxState, setCheckboxState]= useState(
+    new Array(checkboxData.length).fill(false)
+  );
+  const [total, setTotal] = useState(0);
 
-  return (
-    <>
-      
-    </>
-  )
+  const changeState = (index: number) => {
+const newState = checkboxState.map((estado, i)=>{
+if (index===i) {
+  estado = !estado;
+}
+return estado;
+});
+setCheckboxState(newState);
+
+const totalPrice = newState.reduce(
+  (sum, currentState, index)=>{
+    if (currentState === true){
+      return sum + checkboxData[index].price;
+    }
+    return sum;
+  },0);
+  setTotal(totalPrice);
+};
+    return (
+        <>
+            <h4>¿Que quiere hacer?</h4>
+          {checkboxData.map(({text}, index)=>{
+            return(
+              <p key={index}>
+                <input 
+                type="checkbox" 
+                id={`checkbox ${index}`} 
+                checked={checkboxState[index]} 
+                onChange={()=>changeState(index)}
+                /> 
+                <label htmlFor={`checkbox ${index}`}>{text}</label>
+              </p>
+            )
+          })}
+          <p>Total:{total}</p>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/component/checkboxData.tsx
@@ -0,0 +1,14 @@
+export const checkboxData = [
+    {
+        text: "Una página web (500€)",
+        price: 500,
+    },
+    {
+        text: "Una campaña SEO (300€)",
+        price: 300,
+    },
+    {
+        text: "Una campaña de publicidad (200€)",
+        price: 200,
+    },
+];
