d3e7533e1d0b622f170649b27b08ae5239207a33
Dar-ya218 <dasharoura13465@gmail.com>
2023-06-18 18:34:25 +0200

    ejercicio_2

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,38 @@ function App() {
     new Array(checkboxData.length).fill(false)
   );
   const [total, setTotal] = useState(0);
+  const [numPages, setNumPages] = useState(0);
+  const [numLanguages, setNumLanguages] = useState(0);
 
   const changeState = (index: number) => {
 const newState = checkboxState.map((estado, i)=>{
-if (index===i) {
+if (index === i) {
   estado = !estado;
 }
 return estado;
 });
 setCheckboxState(newState);
 
-const totalPrice = newState.reduce(
-  (sum, currentState, index)=>{
+const totalPrice = newState.reduce((sum, currentState, index)=>{
     if (currentState === true){
       return sum + checkboxData[index].price;
     }
     return sum;
   },0);
-  setTotal(totalPrice);
+
+    const webPageCost = numPages * numLanguages * 30;
+
+  setTotal(totalPrice + webPageCost || 0);
+};
+
+const handleNumPagesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const value = parseInt(event.target.value);
+  setNumPages(value || 0);
+};
+
+const handleNumLanguagesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const value = parseInt(event.target.value);
+  setNumLanguages(value || 0);
 };
     return (
         <>
@@ -40,8 +54,30 @@ const totalPrice = newState.reduce(
                 /> 
                 <label htmlFor={`checkbox ${index}`}>{text}</label>
               </p>
-            )
+            );
           })}
+        <div className="adjustment">
+        <h4>Ajustar página web</h4>
+        <p>
+        <label htmlFor="numPages">Número de páginas:</label>
+        <input
+          type="number"
+          id="numPages"
+          value={numPages}
+          onChange={handleNumPagesChange}
+        />
+        </p>
+        <p>
+        <label htmlFor="numLanguages">Número de idiomas:</label>
+        <input
+          type="number"
+          id="numLanguages"
+          value={numLanguages}
+          onChange={handleNumLanguagesChange}
+        />
+        </p>
+      </div>
+      
           <p>Total:{total}</p>
         </>
     );
